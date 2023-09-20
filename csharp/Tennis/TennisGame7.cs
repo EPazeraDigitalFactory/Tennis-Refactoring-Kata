6ae730095f88d422b4b9124bf0c78d6aca39005b
Sensei <eat@joes.com>
2023-09-20 16:15:55 +0000

    commit all of it

---
+++ csharp/Tennis/TennisGame7.cs
@@ -0,0 +1,17 @@
+using System.Collections.Generic;
+
+namespace Tennis
+{
+    public class TennisGame7 : ITennisGame
+    {
+        public string GetScore()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void WonPoint(string playerName)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
